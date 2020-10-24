51eec872679ee3c75181e33d77a86d02fa0332a0
kendrinab <kendra.barrera01@gmail.com>
2020-10-24 19:29:45 -0400

    kendrinab-enhancement-search-form

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import SearchForm from './SearchForm';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <SearchForm />
+    </div>
+  );
+};
+
+export default App;
