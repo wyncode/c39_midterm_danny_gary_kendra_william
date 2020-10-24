5b07a13a3c7fdf4c6a744f5d90654b12922a94c8
wdillard <dillardwa@gmail.com>
2020-10-24 00:07:37 -0500

    added AboutBeatles and SongBox components

---
+++ client/src/components/SongBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+const SongBox = ({}) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img src="holder.js/100px270" alt="Album Cover" />
+      <Card.ImgOverlay>
+        <Card.Title>Song Title</Card.Title>
+        <Card.Subtitle className="album">Album Title</Card.Subtitle>
+        <Card.Text>
+            Some quick example text.
+        </Card.Text>
+        <Card.Link href="#">Guitar Link</Card.Link>
+        <Card.Link href="#">Bass Link</Card.Link>
+        <Card.Link href="#">Drum Link</Card.Link>
+      </Card.ImgOverlay>
+  </Card>
+  );
+};
+
+export default SongBox;
