5fc458ba6230efbb7d777369d0400fae59f489ac
kendrinab <kendra.barrera01@gmail.com>
2020-10-26 14:23:12 -0400

    pull

--- client/src/components/Slideshow.jsx
+++ client/src/components/Slideshow.jsx
@@ -1,12 +1,71 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-
-const Slideshow = () => {
-    return <Carousel>
-        <img src="http://www.quickmeme.com/img/d2/d2a55033bd85f86fdb077fc6f1fb54db0d4543d2c1951a6b174a15d650c8ad90.jpg"></img>
-        <img></img>
-    </Carousel>
-    
+import React, { Component } from 'react';
+import Carousel from '@bit/react-bootstrap.react-bootstrap.carousel';
+import ReactBootstrapStyle from '/react-bootstrap.carousel';
+class Example extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.state = {
+      index: 0,
+      direction: null
+    };
+  }
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction
+    });
+  }
+  render() {
+    const { index, direction } = this.state;
+    return (
+      <Carousel
+        activeIndex={index}
+        direction={direction}
+        onSelect={this.handleSelect}
+      >
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/800/400?text=First slide&bg=373940"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/800/400?text=Second slide&bg=282c34"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/800/400?text=Third slide&bg=20232a"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
 }
-
-export default Slideshow;
+export default () => (
+  <div>
+    <ReactBootstrapStyle />
+    <Example />
+  </div>
+);
