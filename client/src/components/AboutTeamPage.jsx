0a813e33b2808ce1457e0732edef355802817d60
kendrinab <kendra.barrera01@gmail.com>
2020-10-28 15:07:26 -0400

    kendrinab-enhancement-css

--- client/src/components/AboutTeamPage.jsx
+++ client/src/components/AboutTeamPage.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Will from './images/Will.jpg';
 import Danny from './images/Danny.jpg';
 import Gary from './images/gary.jpg';
+import Kendra from './images/Kendra.jpg';
 
 const AboutTeamPage = () => {
   return (
     <>
-      <h1> This is all about the Team! </h1>
+      <h1> The Fab Four </h1>
       <div>
         <img
           style={{
